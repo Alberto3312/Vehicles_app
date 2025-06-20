b8a08e6c988738481b2720dd9c21011aa68b499e
Alberto3312 <albertoreyesbvb11@outlook.com>
2025-06-19 23:01:01 -0600

    actualizacion var X

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ st.header('Análisis de Datos de Vehículos en EE.UU.')
 
 # crear boton para construir histograma usando las funciones st.write y st.plotly_chart
 if st.button('Construir Histograma'):
-    st.write('Histograma de la variable "year"')
-    fig = px.histogram(car_data, x='year',
-                       title='Histograma de Años de Vehículos')
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig)
 
 # crear boton para construir grafico de dispersion usando las funciones st.write y st.plotly_chart
 if st.button('Construir Gráfico de Dispersión'):
-    st.write('Gráfico de Dispersión de "year" vs "mileage"')
-    fig = px.scatter(car_data, x='year', y='mileage',
-                     title='Años vs Kilometraje de Vehículos')
+    st.write('Gráfico de Dispersión de "odometer" vs "price"')
+    fig = px.scatter(car_data, x='odometer', y='price')
     st.plotly_chart(fig)
