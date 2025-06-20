babab16104847fa2dd70ebb569c6771bc5d82079
Alberto3312 <albertoreyesbvb11@outlook.com>
2025-06-19 22:45:26 -0600

    Actualizacion del codigo .py, readme y jupyter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Vehicles_app
 Proyecto TripleTen
+En este proyecto se realizó una aplicación web donde al presionar un botón este crea un histograma o grafico de dispersión del archivo csv que se tiene, en este caso creara un grafico de dispersion de la venta de carros en estados unidos
---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+# crear header
+st.header('Análisis de Datos de Vehículos en EE.UU.')
+
+# crear boton para construir histograma usando las funciones st.write y st.plotly_chart
+if st.button('Construir Histograma'):
+    st.write('Histograma de la variable "year"')
+    fig = px.histogram(car_data, x='year',
+                       title='Histograma de Años de Vehículos')
+    st.plotly_chart(fig)
+
+# crear boton para construir grafico de dispersion usando las funciones st.write y st.plotly_chart
+if st.button('Construir Gráfico de Dispersión'):
+    st.write('Gráfico de Dispersión de "year" vs "mileage"')
+    fig = px.scatter(car_data, x='year', y='mileage',
+                     title='Años vs Kilometraje de Vehículos')
+    st.plotly_chart(fig)
